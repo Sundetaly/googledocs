59b3d900de1f1c7e3b5cec6f8620e44ea42fc8d8
Sundetaly <sundetaliomirzak@gmail.com>
2021-03-16 15:46:39 +0600

    complete view logic

--- app/main.py
+++ app/main.py
@@ -56,6 +56,7 @@ async def create_docs(asterisk_item: schemas.AsteriskItem, settings_cash: settin
     did = asterisk_item.did
     dst = asterisk_item.dst
     called_num = asterisk_item.called_num
+    called_name = asterisk_item.called_name
     wait_time = asterisk_item.wait_time
     talk_time = asterisk_item.talk_time
     wait_time = asterisk_item.wait_time
@@ -86,9 +87,43 @@ async def create_docs(asterisk_item: schemas.AsteriskItem, settings_cash: settin
         ["https://www.googleapis.com/auth/spreadsheets",
          "https://www.googleapis.com/auth/drive"])
     client = gspread.authorize(credentials)
+
     if len(src) > 5:  # ????
         wait_time = time.strftime("%M:%S", time.gmtime(wait_time))
         talk_time = time.strftime("%M:%S", time.gmtime(talk_time))
         did = "".join(i for i in did if i.isdigit())
+
+        if called_num[-10:-7] in settings.MOBILE_PHONE_CODES:
+            if called_num[1] == 7:
+                called_num = '+{}'.format(called_num)
+
+        post_data = [called_date, called_time, did, '', dst, called_num,
+                     called_name, wait_time, talk_time, disposition, who_hungup]
+
+        for i in range(len(post_data)):
+            if isinstance(post_data[i], bytes):
+                post_data[i] = post_data[i].decode("utf-8")
+
+        sheet = client.open(settings.INBOUND_FILE_NAME).sheet1
+        sheet.append_row(post_data)
+
+    else:
+        wait_time = wait_time - talk_time
+        wait_time = time.strftime('%M:%S', time.gmtime(wait_time))
+        talk_time = time.strftime('%M:%S', time.gmtime(talk_time))
+        if did[-10:-7] in did:
+            if did[1] == 7:
+                did = '+7{}'.format(did[-10:])
+        else:
+            did = '+8{}'.format(did[-10:])
+        post_data = [called_date, called_time, did, dst,
+                     called_num, called_name, wait_time, talk_time,
+                     disposition, who_hungup]
+        for i in range(len(post_data)):
+            if isinstance(post_data[i], bytes):
+                post_data[i] = post_data[i].decode("utf-8")
+
+        sheet = client.open(settings.OUTBOUND_FILE_NAME).sheet1
+        sheet.append_row(post_data)
 
     return asterisk_item
