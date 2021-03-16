a20cd1bf0808d5fb515e56833539058849068060
Sundetaly <sundetaliomirzak@gmail.com>
2021-03-16 14:59:19 +0600

    add IntegrityError, HTTPException and refactor

--- app/crud.py
+++ app/crud.py
@@ -3,12 +3,13 @@ from sqlalchemy.orm import Session
 import models, schemas
 
 
-def get_didphonematch(db: Session):
+def get_phonematch(db: Session):
     return db.query(models.DidPhoneMatch).all()
 
 
-def create_didphonematch(db: Session, didphonematch: schemas.DidPhoneMatchCreate):
-    db_phonematch = models.DidPhoneMatch(**didphonematch.dict())
+def create_phonematch(db: Session, phonematch: schemas.DidPhoneMatchCreate):
+
+    db_phonematch = models.DidPhoneMatch(**phonematch.dict())
     db.add(db_phonematch)
     db.commit()
     db.refresh(db_phonematch)
--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
 from typing import List
+from functools import lru_cache
 import datetime
 import time
 from oauth2client.service_account import ServiceAccountCredentials
 import gspread
 
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, HTTPException
+
 from sqlalchemy.orm import Session
-from functools import lru_cache
+from sqlalchemy.exc import IntegrityError
+
 
 from config import settings
 from database import engine, SessionLocal
@@ -22,6 +25,7 @@ app = FastAPI()
 def get_settings():
     return settings
 
+
 def get_db():
     db = SessionLocal()
     try:
@@ -32,42 +36,50 @@ def get_db():
 
 @app.get("/phonematch/", response_model=List[schemas.DidPhoneMatch])
 def read_phonematch(db: Session = Depends(get_db)):
-    return crud.get_didphonematch(db)
+    return crud.get_phonematch(db)
 
 
 @app.post("/phonematch/", response_model=schemas.DidPhoneMatch)
 def create_phonematch(phonematch: schemas.DidPhoneMatchCreate, db: Session = Depends(get_db)):
-    return crud.create_didphonematch(db, didphonematch=phonematch)
+    try:
+        return crud.create_phonematch(db, phonematch=phonematch)
+    except IntegrityError as err:
+        raise HTTPException(status_code=400, detail='Key (did_number)=(string) already exists.')
 
 
 @app.post("/report")
 async def create_docs(asterisk_item: schemas.AsteriskItem, settings_cash: settings = Depends(get_settings)):
     print(settings_cash.CREDENTIALS_FILE)
-    now = datetime.datetime(asterisk_item.called_datetime, "%Y-%m-%d %H:%M:%S")
+    now = datetime.datetime.strptime(asterisk_item.called_datetime, "%Y-%m-%d %H:%M:%S")
     called_date = datetime.date.strftime(now, "%d.%m.%Y")
     called_time = datetime.date.strftime(now, "%H:%M:%S")
     did = asterisk_item.did
+    dst = asterisk_item.dst
     called_num = asterisk_item.called_num
     wait_time = asterisk_item.wait_time
     talk_time = asterisk_item.talk_time
     wait_time = asterisk_item.wait_time
     disposition = asterisk_item.disposition
     src = asterisk_item.src
-    dst = asterisk_item.dst
+
 
     if not talk_time:
         talk_time = 0
+    if not wait_time:
+        wait_time = 0
+
+    if talk_time > 0:
+        disposition = "ANSWERED"
+
     wait_time = int(wait_time)
     talk_time = int(talk_time)
     called_num = "".join(i for i in called_num if i.isdigit())
+
     who_hungup = "..."
     if "SIP" in who_hungup:
         who_hungup = "Оператор"
     elif "IAX2" in who_hungup:
         who_hungup = "Клиент"
-
-    if talk_time > 0:
-        disposition = "ANSWERED"
 
     credentials = ServiceAccountCredentials.from_json_keyfile_name(
         f"{settings_cash.BASE_DIR}/{settings_cash.CREDENTIALS_FILE}",
@@ -78,4 +90,5 @@ async def create_docs(asterisk_item: schemas.AsteriskItem, settings_cash: settin
         wait_time = time.strftime("%M:%S", time.gmtime(wait_time))
         talk_time = time.strftime("%M:%S", time.gmtime(talk_time))
         did = "".join(i for i in did if i.isdigit())
+
     return asterisk_item
