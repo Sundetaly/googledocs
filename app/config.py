d06e9d06adc4f2ce1970521c11bcbf5f0015794e
Sundetaly <sundetaliomirzak@gmail.com>
2021-03-16 13:52:06 +0600

    add pydantic validator to env variables and rename

--- app/config.py
+++ app/config.py
@@ -7,13 +7,13 @@ class Settings(BaseSettings):
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
     # Google docs
-    # CREDENTIALS_FILE = Field(env='CREDENTIALS_FILE')
-    # INBOUND_FILE_NAME = Field(env='INBOUND_FILE_NAME')
-    # OUTBOUND_FILE_NAME = Field(env='OUTBOUND_FILE_NAME')
+    CREDENTIALS_FILE: str
+    INBOUND_FILE_NAME: str
+    OUTBOUND_FILE_NAME: str
 
     class Config:
-        env_file = '.env'
-        env_file_encoding = 'utf-8'
+        env_file = "../.env"
+        env_file_encoding = "utf-8"
         case_sensitive = True
 
 
